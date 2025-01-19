37e22ac2a11387680658a968fba92442369f442f
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-01-19 11:50:40 -0800

    Change range.times to range.each in init_pieces

--- lib/game.rb
+++ lib/game.rb
@@ -16,17 +16,17 @@ class Game
 
   def init_pieces
     pieces = []
-    (0..1).times do |player_index|
+    (0..1).each do |player_index|
       pieces << King.new(player_index, 0)
       pieces << Queen.new(player_index, 0)
 
-      (0..1).times do |piece_index|
+      (0..1).each do |piece_index|
         pieces << Rook.new(player_index, piece_index)
         pieces << Bishop.new(player_index, piece_index)
         pieces << Knight.new(player_index, piece_index)
       end
 
-      (0..7).times { |piece_index| pieces << Pawn.new(player_index, piece_index) }
+      (0..7).each { |piece_index| pieces << Pawn.new(player_index, piece_index) }
     end
   end
 end
