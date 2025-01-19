58834b184b6bd05462b4ba52b80ae2bb089b4dcb
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-01-18 19:09:43 -0800

    Add variables and #initialize to piece

--- lib/piece.rb
+++ lib/piece.rb
@@ -1,2 +1,9 @@
 class Piece
+  attr_reader :type, :player_index, :icon
+
+  def initialize(type, player_index, icon)
+    @type = type
+    @player_index = player_index
+    @icon = icon
+  end
 end
