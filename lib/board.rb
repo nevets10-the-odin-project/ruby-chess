f872ec0cab2cbe74ebdfcb8cedec311bb06cca16
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-01-19 13:36:59 -0800

    Update board#build_spaces, add board#build_column

--- lib/board.rb
+++ lib/board.rb
@@ -4,14 +4,58 @@ class Board
 
   def initialize(pieces)
     @pieces = pieces
-    @spaces = build_spaces
+    @spaces = build_spaces(pieces)
   end
 
-  def build_spaces
-    column = []
-    8.times do
-      column << []
+  def build_spaces(pieces)
+    columns = []
+    (0..7).each do |column_index|
+      case column_index
+      when 0
+        rooks = pieces.filter { |piece| piece.type == 'Rook' && piece.piece_index == 0 }
+        pawns = pieces.filter { |piece| piece.type == 'Pawn' && piece.piece_index == column_index }
+        current_column = build_column(rooks, pawns)
+      when 1
+        knights = pieces.filter { |piece| piece.type == 'Knight' && piece.piece_index == 0 }
+        pawns = pieces.filter { |piece| piece.type == 'Pawn' && piece.piece_index == column_index }
+        current_column = build_column(knights, pawns)
+      when 2
+        bishops = pieces.filter { |piece| piece.type == 'Bishop' && piece.piece_index == 0 }
+        pawns = pieces.filter { |piece| piece.type == 'Pawn' && piece.piece_index == column_index }
+        current_column = build_column(bishops, pawns)
+      when 3
+        queens = pieces.filter { |piece| piece.type == 'Queen' && piece.piece_index == 0 }
+        pawns = pieces.filter { |piece| piece.type == 'Pawn' && piece.piece_index == column_index }
+        current_column = build_column(queens, pawns)
+      when 4
+        kings = pieces.filter { |piece| piece.type == 'King' && piece.piece_index == 0 }
+        pawns = pieces.filter { |piece| piece.type == 'Pawn' && piece.piece_index == column_index }
+        current_column = build_column(kings, pawns)
+      when 5
+        bishops = pieces.filter { |piece| piece.type == 'Bishop' && piece.piece_index == 1 }
+        pawns = pieces.filter { |piece| piece.type == 'Pawn' && piece.piece_index == column_index }
+        current_column = build_column(bishops, pawns)
+      when 6
+        knights = pieces.filter { |piece| piece.type == 'Knight' && piece.piece_index == 1 }
+        pawns = pieces.filter { |piece| piece.type == 'Pawn' && piece.piece_index == column_index }
+        current_column = build_column(knights, pawns)
+      when 7
+        rooks = pieces.filter { |piece| piece.type == 'Rook' && piece.piece_index == 1 }
+        pawns = pieces.filter { |piece| piece.type == 'Pawn' && piece.piece_index == column_index }
+        current_column = build_column(rooks, pawns)
+      end
+      columns << current_column
     end
-    column
+    columns
+  end
+
+  def build_column(specific_piece, pawns)
+    current_column = []
+    current_column << specific_piece[0]
+    current_column << pawns[0]
+    4.times { current_column << nil }
+    current_column << pawns[1]
+    current_column << specific_piece[1]
+    current_column
   end
 end
