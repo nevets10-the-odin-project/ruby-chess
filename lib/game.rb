1810f536c3cefeac417167a0477400db3e1ae4b9
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-01-19 11:23:12 -0800

    Add required classes to game

--- lib/game.rb
+++ lib/game.rb
@@ -1,2 +1,11 @@
+require_relative 'board'
+require_relative 'player'
+require_relative 'king'
+require_relative 'queen'
+require_relative 'rook'
+require_relative 'bishop'
+require_relative 'knight'
+require_relative 'pawn'
+
 class Game
 end
