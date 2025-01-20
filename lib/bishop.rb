e6b371862928806db9015d0f139dcb6272e8a20a
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-01-20 12:53:45 -0800

    Add #filter_moves to bishop

--- lib/bishop.rb
+++ lib/bishop.rb
@@ -5,4 +5,15 @@ class Bishop < Piece
     icon = player_index.zero? ? '♗' : '♝'
     super('Bishop', player_index, piece_index, icon)
   end
+
+  def filter_moves(current_position)
+    possible_moves = []
+    (1..8).each do |step|
+      possible_moves << [current_position[0] + step, current_position[1] + step]
+      possible_moves << [current_position[0] + step, current_position[1] - step]
+      possible_moves << [current_position[0] - step, current_position[1] + step]
+      possible_moves << [current_position[0] - step, current_position[1] - step]
+    end
+    possible_moves.filter { |x, y| x.between?(0..7) && y.between?(0..7) }
+  end
 end
