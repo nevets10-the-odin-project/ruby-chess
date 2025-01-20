b3e7777e90bcf6e701304aba584b3b526e1b2eae
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-01-20 12:58:34 -0800

    Add #filter_moves to queen

--- lib/queen.rb
+++ lib/queen.rb
@@ -5,4 +5,19 @@ class Queen < Piece
     icon = player_index.zero? ? '♕' : '♛'
     super('Queen', player_index, piece_index, icon)
   end
+
+  def filter_moves(current_position)
+    possible_moves = []
+    (1..8).each do |step|
+      possible_moves << [current_position[0] + step, current_position[1]]
+      possible_moves << [current_position[0] - step, current_position[1]]
+      possible_moves << [current_position[0], current_position[1] + step]
+      possible_moves << [current_position[0], current_position[1] - step]
+      possible_moves << [current_position[0] + step, current_position[1] + step]
+      possible_moves << [current_position[0] + step, current_position[1] - step]
+      possible_moves << [current_position[0] - step, current_position[1] + step]
+      possible_moves << [current_position[0] - step, current_position[1] - step]
+    end
+    possible_moves.filter { |x, y| x.between?(0..7) && y.between?(0..7) }
+  end
 end
