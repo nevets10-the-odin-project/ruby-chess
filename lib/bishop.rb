6fa0a0c1bacbb939c17f9718e9fa4a962cdfc8ae
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-01-20 13:50:39 -0800

    Correct #filter_moves

--- lib/bishop.rb
+++ lib/bishop.rb
@@ -14,6 +14,6 @@ class Bishop < Piece
       possible_moves << [current_position[0] - step, current_position[1] + step]
       possible_moves << [current_position[0] - step, current_position[1] - step]
     end
-    possible_moves.filter { |x, y| x.between?(0..7) && y.between?(0..7) }
+    possible_moves.filter { |x, y| x.between?(0, 7) && y.between?(0, 7) }
   end
 end
