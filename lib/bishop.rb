c703edec2dcf270547336ca84b52c8488e425da9
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-01-20 20:31:30 -0800

    Add abbreviation to bishop class

--- lib/bishop.rb
+++ lib/bishop.rb
@@ -3,7 +3,7 @@ require_relative 'piece'
 class Bishop < Piece
   def initialize(player_index, piece_index)
     icon = player_index.zero? ? '♗' : '♝'
-    super('Bishop', player_index, piece_index, icon)
+    super('Bishop', player_index, piece_index, icon, 'b')
   end
 
   def filter_moves(current_position)
