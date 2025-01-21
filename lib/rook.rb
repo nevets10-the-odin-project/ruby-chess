a14d1e4ec618eef7662d7a26f6d162151c9a6b13
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-01-20 20:27:23 -0800

    Add abbreviation to rook class

--- lib/rook.rb
+++ lib/rook.rb
@@ -3,7 +3,7 @@ require_relative 'piece'
 class Rook < Piece
   def initialize(player_index, piece_index)
     icon = player_index.zero? ? '♖' : '♜'
-    super('Rook', player_index, piece_index, icon)
+    super('Rook', player_index, piece_index, icon, 'r')
   end
 
   def filter_moves(current_position)
