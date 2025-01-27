22c35d719cb9e1b809959b3a018a6bdf1a5ae6e8
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-01-26 18:46:22 -0800

    Add #serialize_spaces

--- lib/file_handler.rb
+++ lib/file_handler.rb
@@ -18,9 +18,28 @@ class File_handler
   def to_json(game)
     JSON.dump({
                 current_player: game.current_player,
-                board_spaces: game.board.spaces,
+                board_spaces: serialize_spaces(game.board.spaces),
                 move_history: game.board.move_history
               })
+  end
+
+  def serialize_spaces(spaces)
+    serialized = []
+    spaces.each_with_index do |col, col_index|
+      col.each_with_index do |space, row_index|
+        serialized << if space
+                        {
+                          "#{col_index}#{row_index}": {
+                            type: space.type,
+                            player_index: space.player_index,
+                            piece_index: space.piece_index,
+                            move_count: space.move_count
+                          }
+                        }
+                      end
+      end
+    end
+    serialized
   end
 
   def save(game)
@@ -31,7 +50,7 @@ class File_handler
 
   def load
     file = File.open('save.txt')
-    data = JSON.load(file)
+    data = JSON.parse(file)
     file.close
     data
   end
